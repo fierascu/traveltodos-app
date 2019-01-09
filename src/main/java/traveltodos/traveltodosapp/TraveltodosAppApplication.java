74e4d6fd78186cda3fbf6f0b279b645a58af9087
Geo | Gheorghe Fierascu <fierascu@gmail.com>
2019-01-09 11:29:16 +0200

    Add files via upload

---
+++ src/main/java/traveltodos/traveltodosapp/TraveltodosAppApplication.java
@@ -0,0 +1,14 @@
+package traveltodos.traveltodosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltodosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltodosAppApplication.class, args);
+	}
+
+}
+
