59b62b461a18e1839c3fb05b399729ecce015d57
Camelia Gavrila <camelia.gavrila@nagarro.com>
2019-01-17 10:03:36 +0200

    Add rules file

---
+++ src/main/java/traveltodos/traveltodosapp/TravelPlan.java
@@ -0,0 +1,24 @@
+package traveltodos.traveltodosapp;
+
+public class TravelPlan {
+
+    private String type;
+    private String wanted;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWanted() {
+        return wanted;
+    }
+
+    public void setWanted(String wanted) {
+        this.wanted = wanted;
+    }
+
+}
